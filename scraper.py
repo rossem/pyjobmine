4c037866ff9882783d379f0cd55a242a6f3e80fc
Rossem <rrsbrg@gmail.com>
2015-01-28 00:29:29 -0500

    finished yay

--- scraper.py
+++ scraper.py
@@ -1,116 +1,75 @@
 # WHY IS JOBMINE DOWN AT NIGHT????!?!?!?!?!?!? ugh
+import time
 import requests
 import json
+import sys
 
 import bs4
-
 import mechanize
 
 import selenium
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
 
 
-search_url = "https://jobmine.ccol.uwaterloo.ca/psc/SS/EMPLOYEE/WORK/c/UW_CO_STUDENTS.UW_CO_JOBSRCH" #find this
-login_url = "https://jobmine.ccol.uwaterloo.ca/psp/SS/?cmd=login"
-job_url = "https://jobmine.ccol.uwaterloo.ca/psp/SS_2/EMPLOYEE/WORK/c/UW_CO_STUDENTS.UW_CO_JOBDTLS.GBL?UW_CO_JOB_ID="
+#-----------------------------------------------------------
 
-"""
-br = mechanize.Browser()
-br.set_handle_refresh(False)
+userid = sys.argv[1]
+pwd = sys.argv[2]
 
+search_url = "https://jobmine.ccol.uwaterloo.ca/psc/SS/EMPLOYEE/WORK/c/UW_CO_STUDENTS.UW_CO_JOBSRCH" 
+login_url = "https://jobmine.ccol.uwaterloo.ca/psp/SS/EMPLOYEE/WORK/"
+job_url = "https://jobmine.ccol.uwaterloo.ca/psc/SS_2/EMPLOYEE/WORK/c/UW_CO_STUDENTS.UW_CO_JOBDTLS.GBL?UW_CO_JOB_ID="
 
-def login_jobmine(userid, pwd): #dis works
-    br.open(login_url)          #i think
-    br.select_form('login')
-    br.form['userid'] = userid
-    br.form['pwd'] = pwd
-    br.submit()
-    return br.response()
+browser = webdriver.Firefox()
+job_data_list = []
 
-print login_jobmine("", "")
-
-
-def search_jobs(search_url):
-    job_urls = []
-    br.open(search_url)
-    br.select_form(nr=0)
-    form = br.form
-
-    form['UW_CO_JOBSRCH_UW_CO_WT_SESSION'] = '1155'
-    form['UW_CO_JOBSRCH_UW_CO_ADV_DISCP1'] = ["45"] #sets discipline to softeng
-    #discipline = br.find_control(name="UW_CO_JOBSRCH_UW_CO_ADV_DISCP1")
-    form['UW_CO_JOBSRCH_UW_CO_ADV_DISCP2'] = ["45"]
-    form['UW_CO_JOBSRCH_UW_CO_ADV_DISCP3'] = ["45"]
-    form['UW_CO_JOBSRCH_UW_CO_COOP_JR'] = ['Y']
-    form['UW_CO_JOBSRCH_UW_CO_COOP_INT'] = ['Y']
-    form['UW_CO_JOBSRCH_UW_CO_COOP_SR'] = ['Y']
-    form['TYPE_COOP'] = ['1']
-    form['UW_CO_JOBSRCH_UW_CO_JS_JOBSTATUS'] = ['POST'] #dog
-
-    response = br.submit()
-    soup = bs4.BeautifulSoup(response.read())
-
-    
-
-def get_joblist(list_url):
-    response = requests.get(list_url)
-    soup = bs4.BeautifulSoup(response.text)
-    return [a.attrs.get('href') for a in soup.select('')] #find url to each job
-    
-def get_job_data(job_url):
-    job_data = {}
-    response = requests.get(root_url + job_url)
-    soup = bs4.BeautifulSoup(response.text)
-    #do things to soup and put in job_data
-
-    return job_data 
-
-def main():
-    status_code = login_jobmine(userid, pwd)
-
-    if status_code = requests.codes.ok:
-        job_urls = get_joblist(list_url)
-
-        for job_url in job_urls:
-            get_job_data(job_url)
-
-
-if __name__ == '__main__':
-    #gaycat
-    main()
-
-"""
-
-profile = webdriver.firefox.firefox_profile.FirefoxProfile()
-browser = webdriver.Firefox(firefox_profile=profile)
-
+#-----------------------------------------------------------
 
 def login_jobmine():
     browser.get(login_url)
+    time.sleep(2)
+    old_url = browser.current_url
     browser.find_element_by_id("userid").send_keys(userid)
     browser.find_element_by_id("pwd").send_keys(pwd)
     browser.find_element_by_id("login").find_element_by_xpath("//input[@type='submit'][@name='submit']").submit()
 
-    if 'invalid' or 'required' in browser.page_source:
-        "Please try again."
-        login_jobmine()
+    """
+    Doesnt work :(
+    if old_url == browser.current_url:
+        print "Your username/password are invalid. Please try again."
+        browser.quit()
+        sys.exit(0)
 
-    return
+    """
 
 def search_jobs():
     browser.get(search_url)
-    xpath = "//select[@name='UW_CO_JOBSRCH_UW_CO_ADV_DISCP%s']/option[text()='%s']"
-    browser.find_element_by_xpath(xpath % ('1', 'ENG-Software')).click()#do dis
+    xpath = "//select[@name='UW_CO_JOBSRCH_UW_CO_ADV_DISCP1']/option[text()='%s']"
+    browser.find_element_by_xpath(xpath % 'ENG-Software').click()
     browser.find_element_by_id("UW_CO_JOBSRCH_UW_CO_WT_SESSION").send_keys('1155')
-    browser.find_element_by_id("UW_CO_JOBSRCH_UW_CO_COOP_JR").click()
-    browser.find_element_by_id("UW_CO_JOBSRCH_UW_CO_COOP_INT").click()
-    browser.find_element_by_id("UW_CO_JOBSRCH_UW_CO_COOP_SR").click()
-    driver.find_element_by_id("UW_CO_JOBSRCHDW_UW_CO_DW_SRCHBTN").click()
 
-    return driver.page_source
+    jr = browser.find_element_by_id("UW_CO_JOBSRCH_UW_CO_COOP_JR")
+    inter = browser.find_element_by_id("UW_CO_JOBSRCH_UW_CO_COOP_INT")
+    sr = browser.find_element_by_id("UW_CO_JOBSRCH_UW_CO_COOP_SR")
 
-def get_jobs(html):
+    if (not jr.is_selected()):
+        jr.click()
+
+    if (not inter.is_selected()):
+            inter.click()
+
+    if (not sr.is_selected()):
+            sr.click()
+
+    browser.find_element_by_id("UW_CO_JOBSRCHDW_UW_CO_DW_SRCHBTN").click()
+    time.sleep(4)
+
+
+def get_jobs():
     job_list = []
-    soup = bs4.BeautifulSoup(html)
+    soup = bs4.BeautifulSoup(browser.page_source)
     job_spans = soup.findAll('span', id=lambda x: x and x.startswith('UW_CO_JOBRES_VW_UW_CO_JOB_ID'))
 
     for x in job_spans:
@@ -120,10 +79,49 @@ def get_jobs(html):
 
 
 def scrape_job(job_id):
+    job_data = {}
     browser.get(job_url + job_id)
+    time.sleep(2)
+
+    soup = bs4.BeautifulSoup(browser.page_source)
+
+    job_data['posting open date'] = soup.find(id = "UW_CO_JOBDTL_VW_UW_CO_CHAR_EDATE").text
+    job_data['last day to apply'] = soup.find(id = "UW_CO_JOBDTL_VW_UW_CO_CHAR_DATE").text
+    job_data['emp job #'] = soup.find(id = "UW_CO_JOBDTL_VW_UW_CO_EMPOWN_JOBNO").text
+    job_data['job id'] = job_id
+    job_data['employer'] = soup.find(id = "UW_CO_JOBDTL_DW_UW_CO_EMPUNITDIV").text
+    job_data['job title'] = soup.find(id = "UW_CO_JOBDTL_VW_UW_CO_JOB_TITLE").text
+    job_data['work location'] = soup.find(id = "UW_CO_JOBDTL_VW_UW_CO_WORK_LOCATN").text
+    job_data['grades'] = soup.find(id = "UW_CO_JOBDTL_DW_UW_CO_MARKS_DRVD").text
+    job_data['available openings'] = int(soup.find(id = "UW_CO_JOBDTL_VW_UW_CO_AVAIL_OPENGS").text)
+    job_data['disciplines'] = soup.find(id = "UW_CO_JOBDTL_DW_UW_CO_DESCR").text
+    job_data['disciplines1'] = soup.find(id = "UW_CO_JOBDTL_DW_UW_CO_DESCR100").text
+    job_data['levels'] = soup.find(id = "UW_CO_JOBDTL_DW_UW_CO_DESCR_100").text
+    job_data['hiring process support'] = soup.find(id = "UW_CO_OD_DV2_UW_CO_NAME").text
+    job_data['work term support'] = soup.find(id = "UW_CO_OD_DV2_UW_CO_NAME2").text
+    job_data['comments'] = soup.find(id = "UW_CO_JOBDTL_VW_UW_CO_JOB_SUMMARY").text
+    job_data['job description'] = soup.find(id = "UW_CO_JOBDTL_VW_UW_CO_JOB_DESCR").text
+
+    job_data_list.append(job_data)
+
+    print "Done job: " + job_id
+    print "-------------------------"
 
 
-def get_joblist()
-    html_source = browser.page_source
-    soup = bs4.BeautifulSoup(html_source)
+def main():
+
+    login_jobmine()
+    search_jobs()
+    joblist = get_jobs()
+
+    for job in joblist:
+        scrape_job(job)
+
+    with open('job_data_json', 'w') as outfile:
+        json.dump(job_data_list, outfile, indent=4)
+
+    browser.quit()
+
+
+main()
 
